282f63437b5e280a4bb1d4efbd9b6831c4a16546
Alexander Bernhofer <asedlmayr@web.de>
2025-07-27 07:05:55 +0200

    Cleanup
    Exchange System.out with logger

--- oauth-client/src/main/java/de/drunkenmasters/learning/oauth/client/ClaimNames.java
+++ oauth-client/src/main/java/de/drunkenmasters/learning/oauth/client/ClaimNames.java
@@ -2,6 +2,8 @@ package de.drunkenmasters.learning.oauth.client;
 
 public enum ClaimNames {
     NAME("name"),
+    ACCESS_TOKEN("access_token"),
+    REFRESH_TOKEn("refresh_token"),
     ;
     private String name;
 
--- oauth-client/src/main/java/de/drunkenmasters/learning/oauth/client/TokenFlowWithPkce.java
+++ oauth-client/src/main/java/de/drunkenmasters/learning/oauth/client/TokenFlowWithPkce.java
@@ -3,31 +3,24 @@ package de.drunkenmasters.learning.oauth.client;
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTParser;
 import com.nimbusds.oauth2.sdk.*;
-import com.nimbusds.oauth2.sdk.auth.Secret;
-import com.nimbusds.oauth2.sdk.id.ClientID;
 import com.nimbusds.oauth2.sdk.id.State;
 import com.nimbusds.oauth2.sdk.pkce.CodeChallengeMethod;
 import com.nimbusds.oauth2.sdk.pkce.CodeVerifier;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class TokenFlowWithPkce implements OauthFlow {
-
-
-    private FlowConfig config;
-
-    State state = new State("some-random-state");
-    Scope scope = null;
-
+    private final FlowConfig config;
 
     public TokenFlowWithPkce(FlowConfig config) {
         this.config = config;
@@ -38,20 +31,18 @@ public class TokenFlowWithPkce implements OauthFlow {
     }
 
     public CompletableFuture<JWT> authenticate(Consumer<URI> urlHandler, Scope scope) throws Exception {
-        this.scope = scope;
-//        var codeVerifier = new CodeVerifier("ovoi--1PigftRok-mZc7nK2ii03jnko3pmJl9r97r54");
         var codeVerifier = new CodeVerifier();
-        var requestUri = authenticateWithOpenId(codeVerifier);
-        var jwtToken = startCallback(codeVerifier);
-        System.out.println("Please login: " + requestUri);
+        var state = new State("some random state");
+
+        var jwtToken = startCallback(codeVerifier, scope);
+        var loginUri = buildRequestUri(codeVerifier, scope, state);
+        urlHandler.accept(loginUri);
 
         return jwtToken;
     }
 
-    public URI authenticateWithOpenId(CodeVerifier code) throws Exception {
+    public URI buildRequestUri(CodeVerifier code, Scope scope, State state) {
         var responseType = new ResponseType(ResponseType.Value.CODE);
-//        responseType.add(new ResponseType.Value("id_token"));
-//        responseType.add(ResponseType.Value.TOKEN);
 
         var request = new AuthorizationRequest.Builder(
                 responseType, config.clientId()
@@ -66,12 +57,11 @@ public class TokenFlowWithPkce implements OauthFlow {
         return request.toURI();
     }
 
-    public CompletableFuture<JWT> startCallback(CodeVerifier code) throws Exception {
+    public CompletableFuture<JWT> startCallback(CodeVerifier code, Scope scope) throws Exception {
         var httpServer = HttpServer.create(new InetSocketAddress("localhost", 3000), 0);
         var jwtToken = new CompletableFuture<JWT>();
         Executors.newVirtualThreadPerTaskExecutor().execute(() -> {
-            var callback = Callback.of(httpServer, this, jwtToken);
-            callback.code(code);
+            var callback = Callback.of(httpServer, config, code, scope, jwtToken);
             httpServer.createContext("/callback", callback);
             httpServer.start();
         });
@@ -79,69 +69,60 @@ public class TokenFlowWithPkce implements OauthFlow {
         return jwtToken;
     }
 
-    public JWT requestToken(CodeVerifier codeVerifier, AuthorizationCode code) throws Exception {
-        var request = new TokenRequest(
-                config.authTokenEndpoint(),
-                config.clientId(),
-                new AuthorizationCodeGrant(code, config.callbackEndpoint(), codeVerifier),
-                scope
-        );
-
-        var response = request.toHTTPRequest().send();
-        var jsonResponse = response.getBodyAsJSONObject();
-        var tokenString = jsonResponse.getAsString("access_token");
-        return JWTParser.parse(tokenString);
-    }
-
+    @Slf4j
+    @AllArgsConstructor(staticName = "of")
     public static class Callback implements HttpHandler {
         private final HttpServer server;
-        private final TokenFlowWithPkce flow;
+        private final FlowConfig config;
 
-        private CodeVerifier codeVerifier;
-        private CompletableFuture<JWT> jwtToken;
-
-        private Callback(HttpServer server, TokenFlowWithPkce flow, CompletableFuture<JWT> jwtToken) {
-            this.server = server;
-            this.flow = flow;
-            this.jwtToken = jwtToken;
-        }
-
-        public static Callback of(HttpServer server, TokenFlowWithPkce flow, CompletableFuture<JWT> jwtToken) {
-            return new Callback(server, flow, jwtToken);
-        }
-
-        public void code(CodeVerifier codeVerifier) {
-            this.codeVerifier = codeVerifier;
-        }
+        private final CodeVerifier codeVerifier;
+        private final Scope scope;
+        private final CompletableFuture<JWT> jwtToken;
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            System.out.println("Received: " + exchange.getRequestURI());
+            log.debug("Received: {}", exchange.getRequestURI());
             try {
                 var response = AuthorizationResponse.parse(exchange.getRequestURI());
                 exchange.sendResponseHeaders(200, 0);
                 exchange.getResponseBody().close();
 
-                System.out.println("State: " + response.getState());
-                System.out.println("Success: " + response.indicatesSuccess());
+                log.debug("State: {}", response.getState());
+                log.debug("Success: {}", response.indicatesSuccess());
 
                 var successResponse = response.toSuccessResponse();
                 var code = successResponse.getAuthorizationCode();
-                System.out.println("Code: " + code.getValue());
-                System.out.println("CodeVerifier: " + codeVerifier.getValue());
+                log.debug("Code: {}", code.getValue());
+                log.debug("CodeVerifier: {}", codeVerifier.getValue());
 
-                var jwtToken = flow.requestToken(codeVerifier, code);
-                System.out.println("JWT: " + jwtToken.getParsedString());
-                System.out.println("Name: " + jwtToken.getJWTClaimsSet().getClaim("name"));
+                var jwtToken = requestToken(codeVerifier, code, scope);
+                log.debug("JWT: {}", jwtToken.getParsedString());
+                log.debug("Name: {}", jwtToken.getJWTClaimsSet().getClaim(ClaimNames.NAME.getName()));
                 this.jwtToken.complete(jwtToken);
             } catch (Exception ex) {
-                System.out.println(ex.getMessage());
-                ex.printStackTrace();
+                log.error("Error during authentication", ex);
                 exchange.sendResponseHeaders(400, 0);
                 this.jwtToken.completeExceptionally(ex);
             } finally {
                 exchange.getResponseBody().close();
+                server.stop(0);
             }
         }
+
+        public JWT requestToken(CodeVerifier codeVerifier, AuthorizationCode code, Scope scope) throws Exception {
+            var request = new TokenRequest(
+                    config.authTokenEndpoint(),
+                    config.clientId(),
+                    new AuthorizationCodeGrant(code, config.callbackEndpoint(), codeVerifier),
+                    scope
+            );
+
+            var response = request.toHTTPRequest().send();
+            var jsonResponse = response.getBodyAsJSONObject();
+            log.debug("Response: {}", jsonResponse);
+            var tokenString = jsonResponse.getAsString(ClaimNames.ACCESS_TOKEN.getName());
+            return JWTParser.parse(tokenString);
+        }
+
     }
 }
